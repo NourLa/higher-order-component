149bdfa7368bd0167ef4f966086bc7a8a1717953
NOURLAHWAYA <you@example.com>
2018-11-28 21:16:23 +0200

    Updates

---
+++ precache-manifest.ce70dc8d36c976af95cb0560ccdf3855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ecdbc920b741c679951",
+    "url": "/higher-order-component/static/js/runtime~main.2ecdbc92.js"
+  },
+  {
+    "revision": "9c1d4730b1fd886d1b15",
+    "url": "/higher-order-component/static/js/main.9c1d4730.chunk.js"
+  },
+  {
+    "revision": "55d89170560b1423e635",
+    "url": "/higher-order-component/static/js/1.55d89170.chunk.js"
+  },
+  {
+    "revision": "9c1d4730b1fd886d1b15",
+    "url": "/higher-order-component/static/css/main.1fd4a1df.chunk.css"
+  },
+  {
+    "revision": "f1422258365f1c8dad19b149d7a98f0f",
+    "url": "/higher-order-component/index.html"
+  }
+];
